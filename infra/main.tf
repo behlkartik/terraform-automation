0192b16d9607255b9bc34aa7fd65cfe598f60f88
Kartik Behl <behlkartik20@gmail.com>
2024-05-09 03:02:39 +0530

    Refactored code

--- infra/main.tf
+++ infra/main.tf
@@ -1,11 +1,11 @@
 module "dev" {
-  source        = "${path.module}/dev"
+  source        = "./dev"
   stage         = "dev"
   key_pair_name = "dev-application-server"
 }
 
 module "prod" {
-  source        = "${path.module}/prod"
+  source        = "./prod"
   stage         = "prod"
   key_pair_name = "prod-application-server"
 }
--- terraform_operations.py
+++ terraform_operations.py
@@ -3,6 +3,7 @@ import sys
 import subprocess
 import logging
 from base64 import b64decode
+from argparse import ArgumentParser
 
 logging.basicConfig(
     format="[%(asctime)s] %(levelname)s - %(message)s",
@@ -14,27 +15,40 @@ logger.setLevel(logging.DEBUG)
 
 def run_command(cmd, *args, **kwargs):
     return subprocess.check_output(
-        cmd, shell=True, stderr=subprocess.STDOUT, universal_newlines=True
+        cmd,
+        shell=True,
+        stderr=subprocess.STDOUT,
+        universal_newlines=True,
     )
+
 
 def terraform_init(directory):
     os.chdir(directory)
+    logger.info(f"Directory {directory}")
     return run_command("terraform init")
-    
+
 
 def terraform_plan(directory):
     os.chdir(directory)
+    logger.info(f"Directory {directory}")
     return run_command("terraform plan -out=tfplan")
-    
 
-def process():
-    for dir, subdirs, files in os.walk("."):
+
+def process(directory):
+    for dir, subdirs, files in os.walk(directory):
         if ".terraform" in dir:
-            os.removedirs(".terraform")
-        logger.info(f"Processing terraform files from dir {dir}")
-        terraform_init(dir)
-        terraform_plan(dir)
+            run_command("rm -rf .terraform")
+        if "main.tf" in files:
+            logger.info(f"Processing terraform files from dir {dir}")
+            terraform_init(dir)
+            terraform_plan(dir)
 
 
 if __name__ == "__main__":
-    process(os.getcwd())
+    parser = ArgumentParser(
+        prog=__name__,
+        description="Script to initialize terraform providers and get execution plan",
+    )
+    parser.add_argument("-dir", default=os.path.abspath("./infra"))
+    cmd_line_args = parser.parse_args()
+    process(os.path.abspath(cmd_line_args.dir))
